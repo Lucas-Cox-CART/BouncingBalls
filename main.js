db048d0f3ba8e4f9248d44c2765ca4df01887580
Lucas-Cox-CART <lucas.cox@student.cart.org>
2023-05-18 15:47:51 -0700

    5/18/2023-3:50PM

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
-// setup canvas
-
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
-
-// function to generate random number
-
+const section = document.querySelector('section');
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
+
+const colorStore = ['null', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'white', 'gray', 'brown', 'pink'];
+
+let nut = [];
+let colorPointer = [];
+let positionHeight = [];
+let positionWidth = [];
+let vectorMatrix = [[], [], []];
+// [Speed], [Pos/Neg Height Delta], [Pos/Neg Width Delta]
+
+for (let i = 0; i < 30; i++) {
+  //NUT CREATION
+  nut[i] = document.createElement('div');
+  nut[i].classList.add('nut');
+  section.append(nut[i]);
+  function somethingCool() {
+    for (let i = 0; i < 30; i++) {
+      //NUT STYLE
+      colorPointer[i] = (Math.round(Math.random() * 15));
+      nut[i].style.backgroundColor = colorStore[colorPointer[i]];
+      //NUT POSITION
+      positionHeight[i] = (Math.random() * 100);
+      positionWidth[i] = (Math.random() * 100);
+      nut[i].style.left = positionWidth[i] + "%";
+      nut[i].style.top = positionHeight[i] + "%";
+    }
+  }
+}
+
+setInterval(() => {
+  somethingCool();
+}, 10);

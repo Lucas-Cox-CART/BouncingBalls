942fedf3bdf42300b013852030ac5a7e4b6e5b5c
Lucas-Cox-CART <lucas.cox@student.cart.org>
2023-05-19 15:50:02 -0700

    5/19/2023 3:50PM

--- main.js
+++ main.js
@@ -1,37 +1,52 @@
 const section = document.querySelector('section');
-function random(min, max) {
-  const num = Math.floor(Math.random() * (max - min + 1)) + min;
-  return num;
-}
-
-const colorStore = ['null', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'white', 'gray', 'brown', 'pink'];
-
 let nut = [];
-let colorPointer = [];
-let positionHeight = [];
-let positionWidth = [];
-let vectorMatrix = [[], [], []];
-// [Speed], [Pos/Neg Height Delta], [Pos/Neg Width Delta]
+let color = [];
+let rgba = [];
 
-for (let i = 0; i < 30; i++) {
+for (let i = 0; i < 10; i++) {
   //NUT CREATION
-  nut[i] = document.createElement('div');
-  nut[i].classList.add('nut');
-  section.append(nut[i]);
-  function somethingCool() {
-    for (let i = 0; i < 30; i++) {
-      //NUT STYLE
-      colorPointer[i] = (Math.round(Math.random() * 15));
-      nut[i].style.backgroundColor = colorStore[colorPointer[i]];
-      //NUT POSITION
-      positionHeight[i] = (Math.random() * 100);
-      positionWidth[i] = (Math.random() * 100);
-      nut[i].style.left = positionWidth[i] + "%";
-      nut[i].style.top = positionHeight[i] + "%";
-    }
-  }
-}
+  nut[i] = {
+    root: document.createElement('div'),
+    red: (Math.random()*255),
+    green: (Math.random()*255),
+    blue: (Math.random()*255),
+    left: (Math.random()*100) - 5,
+    top: (Math.random()*100) - 5,
+    directionX: (Math.round(Math.random())),
+    directionY: (Math.round(Math.random()))
+  };
 
-setInterval(() => {
-  somethingCool();
-}, 10);
+  if (nut[i].directionX == 0) {
+    nut[i].directionX = -0.5;
+  } else if (nut[i].directionY == 0) {
+    nut[i].directionY = -0.5;
+  }
+
+  rgba[i] = 'rgba(' + nut[i].red + ',' + nut[i].green + ',' + nut[i].blue + ')';
+  nut[i].root.style.backgroundColor = rgba[i];
+  nut[i].root.style.left = nut[i].left + '%';
+  nut[i].root.style.top = nut[i].top + '%';
+  //NUT COLOR TRANSITION
+  setInterval(() => {colorTransition();}, 10);
+  function colorTransition() {
+    if (nut[i].red >= 255) {
+      nut[i].red = 1;
+    } else if (nut[i].green >= 255) {
+      nut[i].green = 1;
+    } else if (nut[i].blue >= 255) {
+      nut[i].blue = 1;
+    }
+    nut[i].red += 1;
+    nut[i].green += 1;
+    nut[i].blue += 1;
+    rgba[i] = 'rgba(' + nut[i].red + ',' + nut[i].green + ',' + nut[i].blue + ')';
+    nut[i].root.style.backgroundColor = rgba[i];
+  }
+  //NUT PHYSICS
+  setInterval(() => {nutTravel()}, 16.67);
+  function nutTravel() {
+    nut[i].root.style.left = (nut[i].left += nut[i].directionX) + '%';
+    nut[i].root.style.top = (nut[i].top += nut[i].directionY) + '%'
+  }
+  section.append(nut[i].root);
+}

d65fbce25c1d222990b76a92fa0adb8b5ee2404e
Lucas-Cox-CART <lucas.cox@student.cart.org>
2023-05-20 11:02:20 -0700

    5/20/23-11:00AM

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const section = document.querySelector('section');
 const h1 = document.querySelector('h1');
 const nut = [];
+const nutCount = 10; //The # of nuts spawned. Higher #'s cause performance issues. Default: 10
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < nutCount; i++) {
   //NUT TIME DILATION [DEBUG TOOL]
   h1.addEventListener('click', (e) => {
     clearInterval(colorInterval);
@@ -12,17 +13,16 @@ for (let i = 0; i < 10; i++) {
   //NUT GENESIS
   nut[i] = {
     root: document.createElement('div'),
+    size: 1, //Changes size of nuts. Recommended 0.5-3.0. Default: 1
     color: [(Math.random()*255), (Math.random()*255), (Math.random()*255)],
     position: [(Math.random()*100).toFixed(2) - 0, (Math.random()*100).toFixed(2) - 0],
-    direction: [(Math.round(Math.random())), (Math.round(Math.random()))],
-    speed: null
+    vector: Math.random()*360,
   };
-  //NUT FALSE CONVERSION
-  if (nut[i].direction[0] == 0) {
-    nut[i].direction[0] = -1;
-  } else if (nut[i].direction[1] == 0) {
-    nut[i].direction[1] = -1;
-  }
+  //NUT ATTRIBUTE REASSIGNMENT
+  nut[i].root.style.clipPath = `circle(${nut[i].size*25}px at ${nut[i].size*25}px ${nut[i].size*25}px)`;
+  nut[i].root.style.width = `${nut[i].size*50}px`;
+  nut[i].root.style.height = `${nut[i].size*50}px`;
+  nut[i].vector = [Math.cos(nut[i].vector), Math.sin(nut[i].vector)];
   //NUT INITIAL COMPILATION
   nut[i].root.style.backgroundColor = `rgba(${nut[i].color[0]}, ${nut[i].color[1]}, ${nut[i].color[2]})`;
   nut[i].root.style.left = nut[i].position[0] + '%';
@@ -30,8 +30,8 @@ for (let i = 0; i < 10; i++) {
   //NUT SPEED [INCOMPLETE]
   let travelInterval = setInterval(() => {nutTravel();}, 10)
   function nutTravel() {  
-    nut[i].root.style.left = (nut[i].position[0] += nut[i].direction[0]) + '%';
-    nut[i].root.style.top = (nut[i].position[1] += nut[i].direction[1]) + '%';
+    nut[i].root.style.left = (nut[i].position[0] += (1 * nut[i].vector[0])) + '%';
+    nut[i].root.style.top = (nut[i].position[1] += (1 * nut[i].vector[1])) + '%';
   }
   //NUT PHASE SHIFT
   let colorInterval = setInterval(() => {colorTransition();}, 10);
@@ -56,16 +56,35 @@ for (let i = 0; i < 10; i++) {
   function nutBoundary() {
     nutLeftNum[i] = parseInt(nut[i].root.style.left, 10);
     nutTopNum[i] = parseInt(nut[i].root.style.top, 10);
-    if (nutLeftNum[i] >= 100 || nutLeftNum[i] <= 0) {
-      nut[i].direction[0] *= -1;
-    } else if (nutTopNum[i] >= 100 || nutTopNum[i] <= 0) {
-      nut[i].direction[1] *= -1;
+    if (nutLeftNum[i] >= (100 - nut[i].size*1.7) || nutLeftNum[i] <= 0) {
+      nut[i].vector[0] *= -1;
+    } else if (nutTopNum[i] >= (100 - nut[i].size*1.7) || nutTopNum[i] <= 0) {
+      nut[i].vector[1] *= -1;
+    } else if (nutLeftNum[i] > 150 || nutLeftNum[i] < -150) {
+      nut[i].root.style.left = 50 + '%';
+      nut[i].root.style.top = 50 + '%';
+    } else if (nutTopNum[i] > 150 || nutTopNum[i] < -150) {
+      nut[i].root.style.left = 50 + '%';
+      nut[i].root.style.top = 50 + '%';
     }
   }
-  //NUT PHYSICS II: COLLISIONS [HIGH-PRIORITY, PLANNED]
+  //NUT PHYSICS II: COLLISIONS [HIGH-PRIORITY, PLANNED] i'm going to go insane
   setInterval(() => {checkForCollision();}, 10);
   function checkForCollision() {
-
+    for (let a = 1; a < nutCount; a++) {
+      if (Math.abs(nutLeftNum[0] - nutLeftNum[a]) < 3) {
+        nut[0].vector[0] *= -1;
+        nut[a].vector[0] *= -1;
+        console.log(`Collision! At A`);
+      }
+    }
+    for (let b = 2; b < nutCount; b++) {
+      if (Math.abs(nutLeftNum[1] - nutLeftNum[b]) < 3) {
+        nut[1].vector[0] *= -1;
+        nut[b].vector[0] *= -1;
+        console.log(`Collision! At B}`);
+      }
+    }
   }
   //NUT PHYSICS III: GRAVITY [LOW-PRORITY, PLANNED]
   //NUT CHEMISTRY I: FUSION [PLANNED]

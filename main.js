e83e69566d6954b91fbed139c844c2132e4841e6
Lucas-Cox-CART <lucas.cox@student.cart.org>
2023-05-20 08:40:49 -0700

    5/20/2023-8:40AM

--- main.js
+++ main.js
@@ -1,84 +1,74 @@
 const section = document.querySelector('section');
+const h1 = document.querySelector('h1');
 const nut = [];
-const color = [];
-const rgba = [];
 
 for (let i = 0; i < 10; i++) {
+  //NUT TIME DILATION [DEBUG TOOL]
+  h1.addEventListener('click', (e) => {
+    clearInterval(colorInterval);
+    clearInterval(travelInterval);
+    clearInterval(boundaryInterval);
+  });
   //NUT GENESIS
   nut[i] = {
     root: document.createElement('div'),
-    red: (Math.random()*255),
-    green: (Math.random()*255),
-    blue: (Math.random()*255),
-    left: (Math.random()*100).toFixed(2) - 0,
-    top: (Math.random()*100).toFixed(2) - 0,
-    directionX: (Math.round(Math.random())),
-    directionY: (Math.round(Math.random())),
+    color: [(Math.random()*255), (Math.random()*255), (Math.random()*255)],
+    position: [(Math.random()*100).toFixed(2) - 0, (Math.random()*100).toFixed(2) - 0],
+    direction: [(Math.round(Math.random())), (Math.round(Math.random()))],
     speed: null
   };
   //NUT FALSE CONVERSION
-  if (nut[i].directionX == 0) {
-    nut[i].directionX = -1;
-  } else if (nut[i].directionY == 0) {
-    nut[i].directionY = -1;
+  if (nut[i].direction[0] == 0) {
+    nut[i].direction[0] = -1;
+  } else if (nut[i].direction[1] == 0) {
+    nut[i].direction[1] = -1;
   }
   //NUT INITIAL COMPILATION
-  rgba[i] = 'rgba(' + nut[i].red + ',' + nut[i].green + ',' + nut[i].blue + ')';
-  nut[i].root.style.backgroundColor = rgba[i];
-  nut[i].root.style.left = nut[i].left + '%';
-  nut[i].root.style.top = nut[i].top + '%';
+  nut[i].root.style.backgroundColor = `rgba(${nut[i].color[0]}, ${nut[i].color[1]}, ${nut[i].color[2]})`;
+  nut[i].root.style.left = nut[i].position[0] + '%';
+  nut[i].root.style.top = nut[i].position[1] + '%';
+  //NUT SPEED [INCOMPLETE]
+  let travelInterval = setInterval(() => {nutTravel();}, 10)
+  function nutTravel() {  
+    nut[i].root.style.left = (nut[i].position[0] += nut[i].direction[0]) + '%';
+    nut[i].root.style.top = (nut[i].position[1] += nut[i].direction[1]) + '%';
+  }
   //NUT PHASE SHIFT
-  setInterval(() => {colorTransition();}, 10);
+  let colorInterval = setInterval(() => {colorTransition();}, 10);
   function colorTransition() {
-    if (nut[i].red >= 255) {
-      nut[i].red = 1;
-    } else if (nut[i].green >= 255) {
-      nut[i].green = 1;
-    } else if (nut[i].blue >= 255) {
-      nut[i].blue = 1;
+    if (nut[i].color[0] >= 255) {
+      nut[i].color[0] = 1;
+    } else if (nut[i].color[1] >= 255) {
+      nut[i].color[1] = 1;
+    } else if (nut[i].color[2] >= 255) {
+      nut[i].color[2] = 1;
     }
-    nut[i].red += 1;
-    nut[i].green += 1;
-    nut[i].blue += 1;
-    rgba[i] = 'rgba(' + nut[i].red + ',' + nut[i].green + ',' + nut[i].blue + ')';
-    nut[i].root.style.backgroundColor = rgba[i];
-    nut[i].root.style.boxShadow = `20px solid ${rgba[i]})`;
+    nut[i].color[0] += 1;
+    nut[i].color[1] += 1;
+    nut[i].color[2] += 1;
+    nut[i].root.style.backgroundColor = `rgba(${nut[i].color[0]}, ${nut[i].color[1]}, ${nut[i].color[2]})`;
   }
   //NUT RADIANCE [PLANNED]
   //NUT PHYSICS I: BOUNDARIES
-  setInterval(() => {nutTravel();}, 10);
-  function nutTravel() {
-    let nutLeftNum = parseInt(nut[i].root.style.left, 10);
-    let nutTopNum = parseInt(nut[i].root.style.top, 10);
-
-    if (nutLeftNum >= 100) {
-      nut[i].directionX *= -1;
-    } else if (nutLeftNum <= 0) {
-      nut[i].directionX *= -1;
+  let boundaryInterval = setInterval(() => {nutBoundary();}, 10);
+  let nutLeftNum = [];
+  let nutTopNum = [];
+  function nutBoundary() {
+    nutLeftNum[i] = parseInt(nut[i].root.style.left, 10);
+    nutTopNum[i] = parseInt(nut[i].root.style.top, 10);
+    if (nutLeftNum[i] >= 100 || nutLeftNum[i] <= 0) {
+      nut[i].direction[0] *= -1;
+    } else if (nutTopNum[i] >= 100 || nutTopNum[i] <= 0) {
+      nut[i].direction[1] *= -1;
     }
-    if (nutTopNum >= 100) {
-      nut[i].directionY *= -1;
-    } else if (nutTopNum <= 0) {
-      nut[i].directionY *= -1;
-    }
-    nut[i].root.style.left = (nut[i].left += nut[i].directionX) + '%';
-    nut[i].root.style.top = (nut[i].top += nut[i].directionY) + '%';
   }
-  //NUT PHYSICS II: COLLISIONS
+  //NUT PHYSICS II: COLLISIONS [HIGH-PRIORITY, PLANNED]
   setInterval(() => {checkForCollision();}, 10);
-  
   function checkForCollision() {
-    for (let d = 1; d < 10; d++) {
-      if (parseInt(nut[i].root.offsetTop, 10) - parseInt(nut[d].root.offsetTop, 10) < 50) {
-        nut[i].directionY *= -1;
-      } 
-      
-      if (parseInt(nut[i].root.offsetLeft, 10) - parseInt(nut[d].root.offsetLeft, 10) < 50) {
-        nut[i].directionX *= -1;
-      }
-    }
+
   }
-  //NUT PHYSICS III: FUSION [PLANNED]
-  //NUT PHYSICS IV: FISSION [PLANNED]
+  //NUT PHYSICS III: GRAVITY [LOW-PRORITY, PLANNED]
+  //NUT CHEMISTRY I: FUSION [PLANNED]
+  //NUT CHEMISTRY II: FISSION [PLANNED]
   section.append(nut[i].root);
 }

0b7c88ae7e5450f5848112918eee583656072467
Lucas-Cox-CART <lucas.cox@student.cart.org>
2023-05-30 13:19:01 -0700

    5/30/2023 1:20PM

--- main.js
+++ main.js
@@ -47,6 +47,7 @@ nutSizeNumber.addEventListener('change', (e) => {
         document.getElementById('nutSizeNumber').value = "1";
     }
 });
+let selected = [];
 // NUT GRAVITY UI
 let gravityOptions = [];
 for (let o = 0; o < 3; o++) {
@@ -57,16 +58,25 @@ gravityOptions[0].addEventListener('click', (e) => {
     gravityOptions[0].style.animationName = "selectedMode1";
     gravityOptions[1].style.animationName = "selectedMode2Rev";
     gravityOptions[2].style.animationName = "selectedMode3Rev";
+    selected[0] = true;
+    selected[1] = false;
+    selected[2] = false;
 });
 gravityOptions[1].addEventListener('click', (e) => {
     gravityOptions[1].style.animationName = "selectedMode2";
     gravityOptions[0].style.animationName = "selectedMode1Rev";
     gravityOptions[2].style.animationName = "selectedMode3Rev";
+    selected[1] = true;
+    selected[0] = false;
+    selected[2] = false;
 });
 gravityOptions[2].addEventListener('click', (e) => {
     gravityOptions[2].style.animationName = "selectedMode3";
     gravityOptions[0].style.animationName = "selectedMode1Rev";
     gravityOptions[1].style.animationName = "selectedMode2Rev";
+    selected[2] = true;
+    selected[0] = false;
+    selected[1] = false;
 });
 // NUT PHYSICS UI
 let physicsOptions = [];
@@ -78,16 +88,25 @@ physicsOptions[0].addEventListener('click', (e) => {
     physicsOptions[0].style.animationName = "selectedTaut1";
     physicsOptions[1].style.animationName = "selectedTaut2Rev";
     physicsOptions[2].style.animationName = "selectedTaut3Rev";
+    selected[3] = true;
+    selected[4] = false;
+    selected[5] = false;
 });
 physicsOptions[1].addEventListener('click', (e) => {
     physicsOptions[1].style.animationName = "selectedTaut2";
     physicsOptions[0].style.animationName = "selectedTaut1Rev";
     physicsOptions[2].style.animationName = "selectedTaut3Rev";
+    selected[4] = true;
+    selected[3] = false;
+    selected[5] = false;
 });
 physicsOptions[2].addEventListener('click', (e) => {
     physicsOptions[2].style.animationName = "selectedTaut3";
     physicsOptions[0].style.animationName = "selectedTaut1Rev";
     physicsOptions[1].style.animationName = "selectedTaut2Rev";
+    selected[5] = true;
+    selected[3] = false;
+    selected[4] = false;
 });
 
 // NUT CODE
@@ -106,12 +125,119 @@ for (let i = 0; i < nutCount; i++) {
     // NUT GENESIS
     nut[i] = {
         root: document.createElement('div'),
-        size: 1.25,
+        size: 1,
         color: [(Math.random()*255), (Math.random()*255), (Math.random()*255)],
         position: [(Math.random()*100).toFixed(2) - 0, (Math.random()*100).toFixed(2) - 0],
         vector: Math.random()*360
     };
-    // NUT INITIAL COMPILATION
+    initialCompilation(i);
+    let travelInterval = setInterval(() => {nutTravel(i);}, time);
+    let boundaryInterval = setInterval(() => {nutBoundary(i);}, time);
+    let colorInterval = setInterval(() => {colorTransition(i);}, time);
+}
+
+// NUT SPEED
+function nutTravel(i) {  
+    nut[i].root.style.left = ((nut[i].position[0] += (1 * nut[i].vector[0])) / nut[i].size) + '%';
+    nut[i].root.style.top = ((nut[i].position[1] += (1 * nut[i].vector[1])) / nut[i].size) + '%';
+    detectCollision(i);
+}
+
+// NUT COLOR SHIFT
+function colorTransition(i) {
+    if (nut[i].color[0] >= 255) {
+    nut[i].color[0] = 1;
+    } else if (nut[i].color[1] >= 255) {
+    nut[i].color[1] = 1;
+    } else if (nut[i].color[2] >= 255) {
+    nut[i].color[2] = 1;
+    }
+    nut[i].color[0] += 1;
+    nut[i].color[1] += 1;
+    nut[i].color[2] += 1;
+    nut[i].root.style.backgroundColor = `rgba(${nut[i].color[0]}, ${nut[i].color[1]}, ${nut[i].color[2]})`;
+}
+
+// SIMULATION BOUNDARIES
+let nutLeftNum = [];
+let nutTopNum = [];
+function nutBoundary(i) {
+    //SCENE WALL
+    nutLeftNum[i] = (parseFloat(nut[i].root.style.left, 10))*width/100;
+    nutTopNum[i] = (parseFloat(nut[i].root.style.top, 10))*height/100;
+    if (nutLeftNum[i] >= (width - nut[i].size*1.7) || nutLeftNum[i] <= 0) {
+        nut[i].vector[0] *= -1;
+    } else if (nutTopNum[i] >= (height - nut[i].size*1.7) || nutTopNum[i] <= 0) {
+        nut[i].vector[1] *= -1;
+    } 
+    // CELL BOUNDARIES
+    if (nutLeftNum[i] >= width + 5 || nutLeftNum[i] <= -width - 5) {
+        nut[i].root.style.left = ((nut[i].position[0] += (1 * nut[i].vector[0])) / nut[i].size) + '%';
+        nut[i].root.style.top = ((nut[i].position[1] += (1 * nut[i].vector[1])) / nut[i].size) + '%';
+        nut[i].position = [(Math.random()*100).toFixed(2) - 0, (Math.random()*100).toFixed(2) - 0];
+    } else if (nutTopNum[i] >= height + 5 || nutTopNum[i] <= -height - 5) {
+        nut[i].root.style.left = ((nut[i].position[0] += (1 * nut[i].vector[0])) / nut[i].size) + '%';
+        nut[i].root.style.top = ((nut[i].position[1] += (1 * nut[i].vector[1])) / nut[i].size) + '%';
+        nut[i].position = [(Math.random()*100).toFixed(2) - 0, (Math.random()*100).toFixed(2) - 0];
+    }
+}
+
+// NUT PHYSICS: COLLISION
+function detectCollision(i) {
+    const ball1 = nut[i];
+    for (let j = 0; j < nut.length; j++) {
+        if (i !== j) {
+            const ball2 = nut[j];
+            const rect1 = ball1.root.getBoundingClientRect();
+            const rect2 = ball2.root.getBoundingClientRect();
+            if (rect1.left < rect2.right && rect1.right > rect2.left && rect1.top < rect2.bottom && rect1.bottom > rect2.top) {
+                ball1.vector[0] *= -1;
+                ball1.vector[1] *= -1;
+                ball2.vector[0] *= -1;
+                ball2.vector[1] *= -1;
+                if (selected[1] == true) {
+                    gravityLocal(i, j);
+                } else if (selected[2] == true) {
+                    gravityCentral(i);
+                }
+                if (selected[3] == true) {
+                    physicsFusion(i, j);
+                } else if (selected[4] == true) {
+                    physicsFission(i, j);
+                }
+            }
+        }
+    }
+}
+
+// NUT GRAVITY: LOCALIZED [PLANNED]
+function gravityLocal(i, j) {
+
+}
+
+// NUT GRAVITY: CENTRALIZED [PLANNED]
+function gravityCentral() {
+
+}
+
+// NUT PHYSICS: FUSION
+function physicsFusion(i, j) {
+    nut[i].size += (nut[j].size/4);
+    nut[i].root.style.width = `${nut[i].size*50}px`;
+    nut[i].root.style.height = `${nut[i].size*50}px`;
+    nut[i].root.style.clipPath = `circle(${nut[i].size*25}px at ${nut[i].size*25}px ${nut[i].size*25}px)`;
+    if (i !== j) {
+        nut[j].root.remove();
+    }
+}
+
+// NUT PHYSICS: FISSION
+function physicsFission(i, j) {
+    
+}
+
+// NUT INITIAL COMPILATION
+function initialCompilation(i) {
     nut[i].vector = [Math.cos(nut[i].vector), Math.sin(nut[i].vector)];
     nut[i].root.classList.add('nut');
     nut[i].root.style.width = `${nut[i].size*50}px`;
@@ -120,85 +246,5 @@ for (let i = 0; i < nutCount; i++) {
     nut[i].root.style.backgroundColor = `rgba(${nut[i].color[0]}, ${nut[i].color[1]}, ${nut[i].color[2]})`;
     nut[i].root.style.left = nut[i].position[0] + '%';
     nut[i].root.style.top = nut[i].position[1] + '%';
-    // NUT SPEED
-    let travelInterval = setInterval(() => {nutTravel();}, time);
-    function nutTravel() {  
-        nut[i].root.style.left = ((nut[i].position[0] += (1 * nut[i].vector[0])) / nut[i].size) + '%';
-        nut[i].root.style.top = ((nut[i].position[1] += (1 * nut[i].vector[1])) / nut[i].size) + '%';
-        detectCollision();
-    }
-    // NUT COLOR SHIFT
-    let colorInterval = setInterval(() => {colorTransition();}, time);
-    function colorTransition() {
-        if (nut[i].color[0] >= 255) {
-        nut[i].color[0] = 1;
-        } else if (nut[i].color[1] >= 255) {
-        nut[i].color[1] = 1;
-        } else if (nut[i].color[2] >= 255) {
-        nut[i].color[2] = 1;
-        }
-        nut[i].color[0] += 1;
-        nut[i].color[1] += 1;
-        nut[i].color[2] += 1;
-        nut[i].root.style.backgroundColor = `rgba(${nut[i].color[0]}, ${nut[i].color[1]}, ${nut[i].color[2]})`;
-    }
-    // SIMULATION BOUNDARIES
-    let boundaryInterval = setInterval(() => {nutBoundary();}, time);
-    let nutLeftNum = [];
-    let nutTopNum = [];
-    function nutBoundary() {
-        //SCENE WALL
-        nutLeftNum[i] = (parseFloat(nut[i].root.style.left, 10))*width/100;
-        nutTopNum[i] = (parseFloat(nut[i].root.style.top, 10))*height/100;
-        if (nutLeftNum[i] >= (width - nut[i].size*1.7) || nutLeftNum[i] <= 0) {
-            nut[i].vector[0] *= -1;
-        } else if (nutTopNum[i] >= (height - nut[i].size*1.7) || nutTopNum[i] <= 0) {
-            nut[i].vector[1] *= -1;
-        } 
-        // CELL BOUNDARIES
-        if (nutLeftNum[i] >= width + 5 || nutLeftNum[i] <= -width - 5) {
-            nut[i].root.style.left = ((nut[i].position[0] += (1 * nut[i].vector[0])) / nut[i].size) + '%';
-            nut[i].root.style.top = ((nut[i].position[1] += (1 * nut[i].vector[1])) / nut[i].size) + '%';
-            nut[i].position = [(Math.random()*100).toFixed(2) - 0, (Math.random()*100).toFixed(2) - 0];
-        } else if (nutTopNum[i] >= height + 5 || nutTopNum[i] <= -height - 5) {
-            nut[i].root.style.left = ((nut[i].position[0] += (1 * nut[i].vector[0])) / nut[i].size) + '%';
-            nut[i].root.style.top = ((nut[i].position[1] += (1 * nut[i].vector[1])) / nut[i].size) + '%';
-            nut[i].position = [(Math.random()*100).toFixed(2) - 0, (Math.random()*100).toFixed(2) - 0];
-        }
-    }
     section.append(nut[i].root);
-    // NUT PHYSICS: COLLISION
-    function detectCollision() {
-        const ball1 = nut[i];
-        for (let j = 0; j < nut.length; j++) {
-            if (i !== j) {
-                const ball2 = nut[j];
-                const rect1 = ball1.root.getBoundingClientRect();
-                const rect2 = ball2.root.getBoundingClientRect();
-                if (rect1.left < rect2.right && rect1.right > rect2.left && rect1.top < rect2.bottom && rect1.bottom > rect2.top) {
-                    ball1.vector[0] *= -1;
-                    ball1.vector[1] *= -1;
-                    ball2.vector[0] *= -1;
-                    ball2.vector[1] *= -1;
-                    // gravityLocal();
-                    // gravityCentral();
-                    // physicsFusion(j);
-                    // physicsFission(j);
-                }
-            }
-        }
-    }
-    // NUT GRAVITY: LOCALIZED [PLANNED]
-    // NUT GRAVITY: CENTRALIZED [PLANNED]
-    // NUT PHYSICS: FUSION
-    function physicsFusion(j) {
-        nut[i].size += (nut[j].size/6);
-        nut[i].root.style.width = `${nut[i].size*50}px`;
-        nut[i].root.style.height = `${nut[i].size*50}px`;
-        nut[i].root.style.clipPath = `circle(${nut[i].size*25}px at ${nut[i].size*25}px ${nut[i].size*25}px)`;
-        if (i !== j) {
-            nut[j].root.remove();
-        }
-    }
-    // NUT PHYSICS: FISSION [PLANNED]
 }
